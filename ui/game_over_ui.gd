37e274c42c89cd31c0dc98279039eb7c9434e14b
Jordla <jlscoding@gmail.com>
2024-07-05 20:52:24 -0600

    Update game over screen
    Now displays the current score and the best score in two distinct boxes

--- ui/game_over_ui.gd
+++ ui/game_over_ui.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
-@onready var final_score_label = $CenterContainer/VBoxContainer/CenterContainer/FinalScoreLabel
+@onready var current_score_label = $ScoresContainer/CurrentScoreContainer/NinePatchRect/TextContainer/Label
+
+@onready var best_score_label = $ScoresContainer/BestScoreContainer/NinePatchRect/ScoreContainer/Label
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,5 +14,8 @@ func _ready():
 func _process(delta):
 	pass
 
-func display_final_score(score):
-	final_score_label.text = "Your final score was: " + str(score)
+func display_current_score(score):
+	current_score_label.text = str(score)
+
+func display_best_score(score):
+	best_score_label.text = str(score)
